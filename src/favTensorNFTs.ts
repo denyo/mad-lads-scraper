ee6673d906ca40cd47ce5015b7ef89e7ee76246d
Bernd Hacker <hacker.bernd@gmail.com>
2024-09-09 10:49:51 +0200

    feat: extend favTensorNFTs to correctly fav/unfav

--- src/favTensorNFTs.ts
+++ src/favTensorNFTs.ts
@@ -4,45 +4,48 @@ import { favTensorNFT, getTensorFavNFTsForWallet, unfavTensorNFT } from './tenso
 import { Mint } from './models';
 
 const targetDir = path.resolve('./results');
+const resultMintsPath = path.resolve(targetDir, 'result-mints.json');
 const resultMintsWithStreamDataPath = path.resolve(targetDir, 'result-mints-with-stream-data.json');
 
 const favTensorNFTs = async () => {
-  const mints: Mint[] = fs.existsSync(resultMintsWithStreamDataPath)
+  const mints: Mint[] = fs.existsSync(resultMintsPath) ? JSON.parse(fs.readFileSync(resultMintsPath, 'utf8')) : [];
+
+  const mintsWithStreamData: Mint[] = fs.existsSync(resultMintsWithStreamDataPath)
     ? JSON.parse(fs.readFileSync(resultMintsWithStreamDataPath, 'utf8'))
     : [];
-  console.log('mints:', mints.length);
+  console.log('mints:', mintsWithStreamData.length);
 
-  const valuableMints = mints.filter((mint) => mint.claimableWormhole > 2000);
+  const valuableMints = mintsWithStreamData.filter((mint) => mint.claimableWormhole > 2000);
   console.log('valuableMints:', valuableMints.length);
 
   const currentFavs = await getTensorFavNFTsForWallet('Et6B96uSx2wSGLSdj95q1iYncpf43tTPU2mWvCv5s9Ad');
-  // const favsToUnfav = currentFavs.filter((mintId) => !valuableMints.some((item) => item.id === mintId));
-  // console.log('favsToUnfav:', favsToUnfav.length);
+  const madLadsFavs = currentFavs.filter((mintId) => mints.some((item) => item.id === mintId));
 
-  // if (favsToUnfav.length) {
-  //   const results = await Promise.all(
-  //     favsToUnfav.map(async (mintId, i, arr) => {
-  //       await new Promise((resolve) => setTimeout(resolve, i * 200));
-  //       console.log(`calling unfavTensorNFT ${i + 1}/${arr.length} for mint: ${mintId}`);
-  //       return await unfavTensorNFT(mintId);
-  //     }),
-  //   );
+  console.log('madLadsFavs', madLadsFavs.length);
+  const favsToUnfav = madLadsFavs.filter((mintId) => !valuableMints.some((item) => item.id === mintId));
+  console.log('favsToUnfav:', favsToUnfav.length);
 
-  //   console.log(results.map((item) => item[0].data.removeFavMint));
-  // }
+  if (favsToUnfav.length) {
+    const results = await Promise.all(
+      favsToUnfav.map(async (mintId, i, arr) => {
+        await new Promise((resolve) => setTimeout(resolve, i * 200));
+        console.log(`calling unfavTensorNFT ${i + 1}/${arr.length} for mint: ${mintId}`);
+        return await unfavTensorNFT(mintId);
+      }),
+    );
+  }
 
-  const itemsToFav = valuableMints.filter((item) => !currentFavs.includes(item.id)).map((item) => item.id);
+  const itemsToFav = valuableMints.filter((item) => !madLadsFavs.includes(item.id));
   console.log('itemsToFav:', itemsToFav.length);
 
   if (itemsToFav.length) {
     const results = await Promise.all(
-      valuableMints.map(async (item, i, arr) => {
+      itemsToFav.map(async (item, i, arr) => {
         await new Promise((resolve) => setTimeout(resolve, i * 200));
         console.log(`calling ${i + 1}/${arr.length} for mint: ${item.id}`);
         return await favTensorNFT(item.id);
       }),
     );
-    console.log(results.map((item) => item[0].data.addFavMint));
   }
 };
 
